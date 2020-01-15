4425ad4635d517c04e0fb62ceb141da066b60028
79laowang <edward.wang@163.com>
2020-01-15 23:15:40 +0800

    how to add comments

---
+++ basic/comments.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -w
+# -*- coding : utf-8 -*-
+
+# 我是注释，请忽略我。
+
+name = "Madisetti" # 这也是注释
+
+puts name
+
+# block commnets
+# 以下这种块注释会对解释器隐藏 =begin/=end 之间的行：
+=begin
+这是注释。
+这也是注释。
+这也是注释。
+这还是注释。
+=end
+
