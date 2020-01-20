284bd2aa265a18c4c056a78d6e33c80c8d891b09
79laowang <edward.wang@163.com>
2020-01-20 15:44:31 +0800

    OptionParser to cast to an Integer

--- skills/ruby-args.rb
+++ skills/ruby-args.rb
@@ -26,7 +26,19 @@ OptionParser.new do |parser|
   parser.on("-n", "--name NAME", "The name of the person to greet.") do |v|
     options[:name] = v
   end
+  # Note the `Integer` arg. That tells the parser to cast the value to an int.
+  # I could have used `Float`, `Date`, or a number of other types.
+  parser.on("-c", "--count COUNT", Integer, "Repeat the message COUNT times") do |v|
+    options[:count] = v
+  end
 end.parse!
 
 # Now we can use the options hash however we like.
-puts "Hello #{ options[:name] }"  if options[:name]
+# puts "Hello #{ options[:name] }"  if options[:name]
+if options[:name]
+  options.fetch(:count, 1).times do
+    puts "Hello #{ options[:name] }"
+  end
+end
+
+# test: ruby ruby-args.rb -n Starr -c 5
