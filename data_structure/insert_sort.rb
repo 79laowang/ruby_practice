86b32fc2c60da0c840f5cbcdd2b5f07f2609bdee
Ke Wang <50512621+79laowang@users.noreply.github.com>
2020-01-20 08:36:00 +0800

    Update insert_sort.rb

--- data_structure/insert_sort.rb
+++ data_structure/insert_sort.rb
@@ -11,3 +11,9 @@ for i in (0..5)
   print array[i], " "
 end
 
+
+#short syntax
+array = [7,6,5,1,8,9]
+6.times { |i| i.upto(5){ |j|
+        array[i], array[j] = array[j], array[i] while array[i] < array[j] }}
+6.times { |i| print array[i], " "}
