f09a993f492e37e225e875f029b2141c9ba76a83
79laowang <edward.wang@163.com>
2020-01-22 16:28:24 +0800

    traversing dir and file with specified directory

--- skills/file-dir.rb
+++ skills/file-dir.rb
@@ -21,6 +21,42 @@ puts File.extname(__FILE__)
 puts File.basename(__FILE__, ".*")
 puts File.expand_path(File.dirname(__FILE__))
 
+# With a relative path, Dir[] will return relative paths
+# as `[ './myfile', ... ]`
+#
+Dir[ './*' ].select{ |f| File.file? f }.each do |f| 
+  puts f
+end
+
+# Want just the filename?
+# as: [ 'myfile', ... ]
+#
+Dir[ '../*' ].select{ |f| File.file? f }.map{ |f| File.basename f }.each do |f|
+  puts f
+end
+
+# Turn them into absolute paths?
+# [ '/path/to/myfile', ... ]
+#
+Dir[ '../*' ].select{ |f| File.file? f }.map{ |f| File.absolute_path f }.each do |f|
+  puts f
+end
+
+# With an absolute path, Dir[] will return absolute paths:
+# as: [ '/var/../var/test/myfile', ... ]
+#
+Dir[ '/var/../var/log/*' ].select{ |f| File.file? f }.each do |f|
+  puts f
+end
+
+# Need the paths to be canonical?
+# as: [ '/var/test/myfile', ... ]
+#
+Dir[ '/var/../var/log/*' ].select{ |f| File.file? f }.map{ |f| File.expand_path f }.each do |f|
+  puts f
+end
+
+
 Net::HTTP.start("fedora.inode.at") do |http|
     resp = http.get("/releases/30/COMPOSE_ID")
     open("COMPOSE_ID", "wb") do |file|
