b28e04f392ce98193e8792249252756017d8bd21
79laowang <edward.wang@163.com>
2020-01-23 09:34:44 +0800

    Print some message

--- data_structure/insert_sort.rb
+++ data_structure/insert_sort.rb
@@ -1,19 +1,27 @@
 #!/usr/bin/ruby
+puts  "Insertion sorting:"
 array = [7, 6, 5, 1, 8, 9]
-
-for i in (0..5)
-  for j in (i..5)
+puts "Original array:"
+for i in (0...array.length)
+  print array[i], " "
+end
+puts 
+puts "Sorted array:"
+for i in (0...array.length)
+  for j in (i...array.length)
     array[i], array[j] = array[j], array[i] while array[i] < array[j]
   end
 end
 
-for i in (0..5)
+for i in (0...array.length)
   print array[i], " "
 end
 
-
+puts
+puts "-"*20
+puts "Another sorted array method:"
 #short syntax
 array = [7,6,5,1,8,9]
-6.times { |i| i.upto(5){ |j|
+array.length.times { |i| i.upto(5){ |j|
         array[i], array[j] = array[j], array[i] while array[i] < array[j] }}
-6.times { |i| print array[i], " "}
+array.length.times { |i| print array[i], " "}
